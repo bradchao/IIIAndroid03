cf363f4affff49f40964b014f6b9e506727ce9bb
brad <brad@brad.tw>
2019-11-08 16:47:32 +0800

    5

--- app/src/main/java/tw/org/iii/iiiandroid03/WelcomeActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid03/WelcomeActivity.java
@@ -2,7 +2,10 @@ package tw.org.iii.iiiandroid03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void gotoMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("brad", "Welcome die");
     }
 }
